929e5cf8dd28827fcca7c3079caf77041c957634
okorkov <alex.okorkov@gmail.com>
2020-11-12 15:07:36 -0800

    commit tree 79c43fa89b1ca822f0a14206647e354bb31a5185

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :match
+    def initialize(word)
+        @word = word.split('').sort
+    end
+    def match(array)
+      result = Array.new
+      word = @word
+      array.each do |element|
+        if element.split('').sort == word
+          result << element
+        end
+      end
+      result
+    end
+end
